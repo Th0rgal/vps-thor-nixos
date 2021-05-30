705a0354308e505c1ea38a388903f7b9d8863dc3
Thomas Marchand <me@thomas-marchand.com>
2021-05-30 09:47:02 +0200

    add Louis a dit

--- services/louis-a-dit.nix
+++ services/louis-a-dit.nix
@@ -1,17 +1,17 @@
 { pkgs, ... }:
 let
   http-python = pkgs.python3.withPackages
-    (python-packages: with python-packages; [ aiohttp ]);
+    (python-packages: with python-packages; [ discordpy pillow toml ]);
 in {
-  systemd.services.travelling-nimtoshi = {
+  systemd.services.louis-a-dit = {
     description = "PROGRAMMESWAG";
     after = [ "network.target" ];
 
     serviceConfig = {
       Type = "simple";
       User = "thomas";
-      ExecStart = http-python + "/bin/python ./main.py";
-      WorkingDirectory = "/home/thomas/services/travelling-nimtoshi/";
+      ExecStart = http-python + "/bin/python ./louis.py";
+      WorkingDirectory = "/home/thomas/services/louis-a-dit/";
       Restart = "on-failure";
     };
 
